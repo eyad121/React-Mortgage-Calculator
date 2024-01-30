c7632a555ba13a2ed25d06587aee5946abd397dd
eyad121 <aeyad3470@gmail.com>
2024-01-30 12:44:57 -0800

    first commit

---
+++ src/theme.js
@@ -0,0 +1,9 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+export  const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
